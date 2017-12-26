23e783c8dc250fcf70de6220c7306840282d1193
Dell Schulz <dellschulz@gmail.com>
2017-12-25 23:50:11 -0200

    Update and rename liquid.mk to pure.mk

--- pure.mk
+++ pure.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+$(call inherit-product, vendor/pure/config/common_full_phone.mk)
 
-PRODUCT_NAME := liquid_mido
+PRODUCT_NAME := pure_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
